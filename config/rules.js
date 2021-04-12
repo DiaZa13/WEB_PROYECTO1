05bc322133b205d650f6a8155f354af44ebb6f16
DiaZa13 <cor191025@uvg.edu.gt>
2021-04-12 01:06:18 -0600

    Initial commit

---
+++ config/rules.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+    },
+  },
+  {
+    test: /\.(svg|woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          outputPath: 'fonts',
+        },
+      },
+    ],
+  },
+  {
+    test: /\.css$/,
+    use: [
+      {
+        loader: 'style-loader',
+      },
+      {
+        loader: 'css-loader',
+      },
+    ],
+  },
+];
