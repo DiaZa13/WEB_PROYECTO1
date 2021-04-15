79e18b0b8a7130246512668bcc2843ef90c966ae
Zaray <cor191025@uvg.edu.gt>
2021-04-15 09:48:27 -0600

    updating style and scroll items array

---
+++ src/utils/scrollItems.jsx
@@ -0,0 +1,130 @@
+import img0 from '../assets/overflow.png';
+import img1 from '../assets/overflowa.png';
+import img2 from '../assets/overflowb.png';
+import img3 from '../assets/overflowc.png';
+import img4 from '../assets/overflowd.png';
+import img5 from '../assets/overflowe.png';
+import img6 from '../assets/overflowf.png';
+import img7 from '../assets/overflowg.png';
+import img8 from '../assets/overflowh.png';
+import img9 from '../assets/overflowi.png';
+import img10 from '../assets/overflowj.png';
+import img11 from '../assets/overflowk.png';
+import img12 from '../assets/overflowl.png';
+import img13 from '../assets/overflowm.png';
+import img14 from '../assets/overflown.png';
+import img15 from '../assets/overflowo.png';
+import img16 from '../assets/overflowp.png';
+import img17 from '../assets/overflowq.png';
+import img18 from '../assets/overflowr.png';
+import img19 from '../assets/overflows.png';
+import img20 from '../assets/overflowt.png';
+import img21 from '../assets/overflowu.png';
+import img22 from '../assets/overflowv.png';
+import img23 from '../assets/overfloww.png';
+import img24 from '../assets/overflowx.png';
+
+export default () => {
+  const items = [];
+  items.push({
+    img: img0,
+    description: 'Pixel',
+  });
+  items.push({
+    img: img1,
+    description: 'Pixel 4a with 5G',
+  });
+  items.push({
+    img: img2,
+    description: 'Nest Audio',
+  });
+  items.push({
+    img: img3,
+    description: 'Chromecast with Google TV',
+  });
+  items.push({
+    img: img4,
+    description: 'Nest Thermostat',
+  });
+  items.push({
+    img: img5,
+    description: 'Fitbit Versa 3',
+  });
+  items.push({
+    img: img6,
+    description: 'Pixel Buds',
+  });
+  items.push({
+    img: img7,
+    description: 'Nest Wifi',
+  });
+  items.push({
+    img: img8,
+    description: 'Nest Hub (2nd gen)',
+  });
+  items.push({
+    img: img9,
+    description: 'Nest Hub Max',
+  });
+  items.push({
+    img: img10,
+    description: 'Pixelbook Go',
+  });
+  items.push({
+    img: img11,
+    description: 'Chromecast',
+  });
+  items.push({
+    img: img12,
+    description: 'Nest Mini (2nd Gen)',
+  });
+  items.push({
+    img: img13,
+    description: 'Stadia Premiere Edition',
+  });
+  items.push({
+    img: img14,
+    description: 'Nest Learning Thermostat',
+  });
+  items.push({
+    img: img15,
+    description: 'Nest Hello Doorbell',
+  });
+  items.push({
+    img: img16,
+    description: 'Google Wifi',
+  });
+  items.push({
+    img: img17,
+    description: 'Nest Protect',
+  });
+  items.push({
+    img: img18,
+    description: 'Nest Cam IQ Indoor',
+  });
+  items.push({
+    img: img19,
+    description: 'Nest Cam Indoor',
+  });
+  items.push({
+    img: img20,
+    description: 'Nest Cam IQ Outdoor',
+  });
+  items.push({
+    img: img21,
+    description: 'Nest Cam Outdoor',
+  });
+  items.push({
+    img: img22,
+    description: 'Nest Yale Lock',
+  });
+  items.push({
+    img: img23,
+    description: 'Nest Secure Alarm',
+  });
+  items.push({
+    img: img24,
+    description: 'Nest Temperature Sensor',
+  });
+  return items;
+};
