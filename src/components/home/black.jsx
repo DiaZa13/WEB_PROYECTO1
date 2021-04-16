6e4dbebba2d59a96ba660bf23c0069a6ba2b51a5
Zaray <cor191025@uvg.edu.gt>
2021-04-16 03:23:30 -0600

    black message

--- src/components/home/black.jsx
+++ src/components/home/black.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function functionName() {
+function Black() {
   return (
-    <div>
+    <div className="pt-24">
+      <div className="bg-black">
+        <div className="products-container-p product-description-container">
+          <div className="black-container">
+            <div className="black-description text-secondary">
+              Google is committed to advancing racial equity for Black community
+            </div>
+            <div style={{ height: '60px' }} className="d-flex align-items-end justify-content-center">
+              <span className="text-light-blue more">
+                See how
+                <div className="line" />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   );
 }
 
-export default functionName;
+export default Black;
